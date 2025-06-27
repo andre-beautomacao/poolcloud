a736f18fef41934a39b55e385ac8daded7f4c2fb
andre-beautomacao <andre.silva@behome.com.br>
2025-06-27 15:01:00 -0300

    fix piscina list duplication

--- backend/listar_piscinas.php
+++ backend/listar_piscinas.php
@@ -16,37 +16,16 @@ $enderecoID = isset($_GET['endereco_id']) ? intval($_GET['endereco_id']) : null;
 
 try {
     $sql = "
-        SELECT 
+        SELECT
             p.id AS piscina_id,
             p.nome AS piscina_nome,
             e.nome AS endereco_nome,
-            p.data_hora,
-            d.ph AS ph,
-            d.orp AS orp,
-            d.temperatura AS temperatura,
-            d.setpoint,
-            d.digipot,
-            d.tensao,
-            d.corrente,
-            d.temp_habilitada,
-            d.data_hora AS data_hora_automatic,
+            p.data_hora
 
-            -- Entradas digitais
-            d.di01_nome, d.di01_tipo, d.di01_status,
-            d.di02_nome, d.di02_tipo, d.di02_status,
-            d.di03_nome, d.di03_tipo, d.di03_status,
-            d.di04_nome, d.di04_tipo, d.di04_status,
-            d.di05_nome, d.di05_tipo, d.di05_status,
-            d.di06_nome, d.di06_tipo, d.di06_status,
-            d.di07_nome, d.di07_tipo, d.di07_status,
-            d.di08_nome, d.di08_tipo, d.di08_status
-
-        FROM 
+        FROM
             piscinas p
-        JOIN 
+        JOIN
             enderecos e ON p.endereco_id = e.id
-        LEFT JOIN 
-            dispositivos d ON p.id = d.piscina_id
         WHERE 1 = 1
     ";
 
